3b0dd0aedf0a53671932f815eafcddbf534d55de
Bala <apple@MacBookPro.communityfibre.co.uk>
2026-05-12 13:57:46 +0100

    First GDS prototype

--- app/routes.js
+++ app/routes.js
@@ -7,3 +7,11 @@ const govukPrototypeKit = require('govuk-prototype-kit')
 const router = govukPrototypeKit.requests.setupRouter()
 
 // Add your routes here
+router.post('/eligible-answer', function(req, res) {
+  var over18 = req.session.data['over-18']
+  if (over18 === 'yes') {
+    res.redirect('/check-answers')
+  } else {
+    res.redirect('/ineligible')
+  }
+})
